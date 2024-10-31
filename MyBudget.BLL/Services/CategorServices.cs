ecb482891359460c28fcfff23d651238d84ba2d5
samchuk-maxim <mk2b79@gmail.com>
2024-10-31 21:26:03 +0200

    Add BLL and base Services

---
+++ MyBudget.BLL/Services/CategorServices.cs
@@ -0,0 +1,37 @@
+﻿using MyBudget.DAL.Entities;
+
+namespace MyBudget.BLL.Services
+{
+    internal class CategorServices
+    {
+        public CategorServices()
+        {
+
+        }
+
+        public Task<List<Category>> GetCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetCategoryByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddCategorAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategorAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
