db7f80ade5c936fe177b84082881a84dca1569a4
samchuk-maxim <mk2b79@gmail.com>
2024-10-31 21:25:16 +0200

    Add DAL and base entities

---
+++ MyBudget.DAL/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBudget.DAL.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        //public List<Record>? Record { get; set; }
+    }
+}
---
+++ MyBudget.DAL/Entities/Record.cs
@@ -0,0 +1,12 @@
+﻿namespace MyBudget.DAL.Entities
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public decimal Money { get; set; }
+        public Category? Category { get; set; }
+        public string? Description { get; set; }
+
+        //public int CategoryId {  get; set; }
+    }
+}
