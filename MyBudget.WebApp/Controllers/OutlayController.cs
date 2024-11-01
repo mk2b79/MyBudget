826ac7bf9bf9ecd76d15c27acf442a81fe8ebf90
mk2b79 <samchuk.maks@outlook.com>
2024-11-01 20:05:43 +0200

    Add WebApp, OutlayController and views

---
+++ MyBudget.WebApp/Controllers/OutlayController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBudget.DAL.Entities;
+
+namespace MyBudget.WebApp.Controllers
+{
+    public class OutlayController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Outlay> outlays = new List<Outlay>();
+
+            outlays.Add(new Outlay() { Id = 1,Money=200,Category=null, Description="Test outlay 1"});
+            outlays.Add(new Outlay() { Id = 2,Money=2123,Category=null, Description="Test outlay 2"});
+            outlays.Add(new Outlay() { Id = 3,Money=1233,Category=null, Description="Test outlay 3"});
+            outlays.Add(new Outlay() { Id = 4,Money=1235343,Category=null, Description="Test outlay 4"});
+
+            return View(outlays);
+        }
+
+        public IActionResult AddOutlay()
+        {
+            return View();
+        }
+        //public IActionResult AddOutlay(Outlay outlay)
+        //{
+        //    return View();
+        //}
+    }
+}
